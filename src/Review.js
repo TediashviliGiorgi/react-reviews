4375b0cb2c8dee8eaf84e5a5cf02dfc1cd8272f9
TediashviliGiorgi <personalmailgt@yahoo.com>
2021-04-26 01:32:49 +0400

    initial commit

---
+++ src/Review.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+  const [index, setIndex] = useState (0)
+  const {name, job, image, text} = people[index]
+
+
+
+  const nextPerson = () =>{
+    setIndex ((index) => {
+      var newIndex = index + 1
+      return newIndex
+    })
+  } 
+
+  var prevPerson = () =>{
+    setIndex ((index) => {
+      var newIndex = index + 1
+      return newIndex
+    })
+    
+  } 
+
+  
+
+  
+
+  return (
+    <article className  = 'review' >
+      <div className = 'img-container'>
+        <img src = {image} alt = {name} className = 'person-img'></img>
+      <span className = 'quote-icon'>
+        <FaQuoteRight />
+      </span>
+      </div>
+      <h4 className = 'author'> { name } </h4>
+      <p className = 'job'> { job } </p>
+      <p className = 'info'> { text } </p>
+      <div className = 'button-container'>
+        <button className = 'prev-btn' onClick = {prevPerson} >
+          <FaChevronLeft />
+        </button>
+        <button className = 'next-btn' onClick = {nextPerson}>
+          <FaChevronRight  />
+        </button>
+      </div>
+      <button className = 'random-btn' > Surprise me </button>
+      
+    </article>
+  )
+};
+
+export default Review;
